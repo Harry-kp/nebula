6e6bf27498c7814c269760a650c3817645bb8f02
Harshit Chaudhary <harshit.chaudhary@procol.in>
2024-08-16 22:43:57 +0530

    Pass 3rd Stage

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -5,37 +5,91 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"unicode"
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
-// Example:
-// - 5:hello -> hello
-// - 10:hello12345 -> hello12345
-func decodeBencode(bencodedString string) (interface{}, error) {
-	if unicode.IsDigit(rune(bencodedString[0])) {
-		var firstColonIndex int
-
-		for i := 0; i < len(bencodedString); i++ {
-			if bencodedString[i] == ':' {
-				firstColonIndex = i
-				break
-			}
-		}
-
-		lengthStr := bencodedString[:firstColonIndex]
-
-		length, err := strconv.Atoi(lengthStr)
-		if err != nil {
-			return "", err
-		}
-
-		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-	} else if bencodedString[0] == 'i' && bencodedString[len(bencodedString)-1] == 'e' {
-		return strconv.Atoi(bencodedString[1 : len(bencodedString)-1])
-	} else {
-		return "", fmt.Errorf("Only strings are supported at the moment")
+func decodeBencode(bencodedString string, start int) (interface{}, int, error) {
+	if start >= len(bencodedString) {
+		return "", start, fmt.Errorf("Invalid Bencoded string")
 	}
+
+	if len(bencodedString) == 0 {
+		return "", start, fmt.Errorf("Invalid Bencoded string")
+	}
+
+	if bencodedString[start] == 'l' {
+		return decodeList(bencodedString, start)
+	} else if bencodedString[start] == 'i' {
+		return decodeInt(bencodedString, start)
+	} else if bencodedString[start] >= '0' && bencodedString[start] <= '9' {
+		return decodeString(bencodedString, start)
+	} else {
+		return "", start, fmt.Errorf("Invalid Bencoded string")
+	}
+}
+
+func decodeList(bencodedString string, start int) (interface{}, int, error) {
+	start++
+	result := make([]interface{}, 0)
+
+	for start < len(bencodedString) && bencodedString[start] != 'e' {
+		var decoded interface{}
+		var err error
+
+		decoded, start, err = decodeBencode(bencodedString, start)
+		if err != nil {
+			return "", start, err
+		}
+
+		result = append(result, decoded)
+	}
+	return result, start + 1, nil
+}
+
+func decodeInt(bencodedString string, start int) (interface{}, int, error) {
+	start++
+	end := start
+
+	for end < len(bencodedString) && bencodedString[end] != 'e' {
+		end++
+	}
+
+	if end == len(bencodedString) {
+		return "", start, fmt.Errorf("Invalid Bencoded string")
+	}
+
+	value, err := strconv.Atoi(bencodedString[start:end])
+	if err != nil {
+		return "", start, err
+	}
+
+	return value, end + 1, nil
+}
+
+func decodeString(bencodedString string, start int) (interface{}, int, error) {
+	colonIndex := start
+
+	for colonIndex < len(bencodedString) && bencodedString[colonIndex] != ':' {
+		colonIndex++
+	}
+
+	if colonIndex == len(bencodedString) {
+		return "", start, fmt.Errorf("Invalid Bencoded string")
+	}
+
+	length, err := strconv.Atoi(bencodedString[start:colonIndex])
+	if err != nil {
+		return "", start, err
+	}
+
+	start = colonIndex + 1
+	end := start + length
+
+	if end > len(bencodedString) {
+		return "", start, fmt.Errorf("Invalid Bencoded string")
+	}
+
+	return bencodedString[start:end], end, nil
 }
 
 func main() {
@@ -44,7 +98,7 @@ func main() {
 	if command == "decode" {
 		bencodedValue := os.Args[2]
 
-		decoded, err := decodeBencode(bencodedValue)
+		decoded, _, err := decodeBencode(bencodedValue, 0)
 		if err != nil {
 			fmt.Println(err)
 			return
